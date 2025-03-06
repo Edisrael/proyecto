77bda248662841cd3b9acb51541c87283c4c669d
JulianGil2025 <gil.julian2025@gmail.com>
2025-03-06 17:27:25 -0600

    Codigo PHP
    cambios correctos
    
    Fecha y hora en php
    
    Dias de la semana html
    
    Se agrego el año PHP
    
    se añaden parrafos
    
    se reubica un ciclo for
    
    se rehubican algunas funcionalidades y se modifica un ciclo en el
    proyecto
    
    cambios correctos
    
    cambios en los ciclos y se agrega lorem
    
    Se anexan nuevos ciclos al proyecto
    
    Se anexan nuevos textos y se modifican algunas cosas
    
    Parrado de La Casa de los Azulejos
    
    se agregan nuevas funciones
    
    unico commit

--- practica_git.php
+++ practica_git.php
@@ -7,6 +7,82 @@
 </head>
 <body>
     <p>Mi rama</p>
+    <h2>esto es un cambio </h2>
+    <?php
+    
+    $i = 10;
+
+    if ($i < 20) {
+    echo "<br>";
+        for ($i=1; $i < 10 ; $i++) { 
+            echo "<br> $i ";
+            for ($e=1; $e < 20 ; $e++) { 
+                echo " $e ";
+            }
+        
+            echo 
+            '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+            aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+            aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>';
+        } 
+    }else{
+        for ($i=1; $i < 10 ; $i++) { 
+            echo "<br> $i ";
+        for ($e=1; $e < 20 ; $e++) { 
+            echo " $e ";
+        } 
+        
+        }
+    }
+    echo "</h1>";
+?> 
+ <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+ <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+ <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+<?php
+    
+    $i = 10;
+
+    if ($i < 20) {
+    echo "<br>";
+        for ($i=1; $i < 10 ; $i++) { 
+            echo "<br> $i ";
+            for ($e=1; $e < 20 ; $e++) { 
+                echo " $e ";
+            } 
+
+        } 
+    }else{
+        for ($i=1; $i < 10 ; $i++) { 
+            echo "<br> $i ";
+        for ($e=1; $e < 20 ; $e++) { 
+            echo " $e ";
+        } 
+        
+        }
+    }
+    echo "</h1>";
+?> 
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+<h1>Hola estoy aqui</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+<h1>Hola estoy aqui</h1>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur, quo rem, 
+ aliquid possimus tenetur quia voluptatem deleniti inventore sed nisi adipisci doloremque tempora sit optio modi consequatur aut nostrum id!</p>
+<h1>Hola estoy aqui</h1>
+
+
+<h2>La Casa de los Azulejos: La historia de amor prohibido que pocos conocen</h2>
+<p>La Casa de los Azulejos es uno de los lugares más bellos e icónicos de la Ciudad,
+     pero muy pocas personas saben que esta joya arquitectónica guarda distintas leyendas entre sus muros, 
+    y que solo algunas de ellas son conocidas.</p>
     
 </body>
 </html>
